d7c7f7569fd6437f6614e39c7c886a03208e33ee
Tankybara <147439760+Tankybara@users.noreply.github.com>
2023-10-23 19:51:43 +0000

    hecho

--- entrega/Ejercicios/Ejercicio2.py
+++ entrega/Ejercicios/Ejercicio2.py
@@ -6,13 +6,13 @@ print(6**2)
 print(3//2)
 x = 8
 ecua = x*2+6*x+9
-print(ecua)
+print("Prueba 1:",ecua)
 x = 20
 ecua = x*2+6*x+9
-print(ecua)
+print("Prueba 2:",ecua)
 x = 87
 ecua = x*2+6*x+9
-print(ecua)
+print("Prueba 3:",ecua)
 x = -43
 ecua = x*2+6*x+9
-print(ecua)
+print("Prueba 4:",ecua)
---
+++ entrega/Ejercicios/Ejercicio3.py
@@ -0,0 +1,9 @@
+miNombre = "Maximiliano"
+miApellido = "Alvarez"
+miEdad = 21
+miAltura = 169
+numeroVuelo = 26611
+temperaturaDelAmbiente = -4.0
+salarioMensual = 153450.96
+gameOver = False
+esPar = True
