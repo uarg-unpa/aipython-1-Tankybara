41b599371f8d2d80d8b515767e34f43dbc5210f5
Tankybara <147439760+Tankybara@users.noreply.github.com>
2023-11-03 22:06:27 +0000

    eo

---
+++ entrega/iteraciones.py
@@ -0,0 +1,26 @@
+#cont=0
+#while cont<=10:
+#    print(cont)
+#    cont=cont+1
+
+#Promedio de notas
+#cont=1
+#suma=0
+#while cont <= 3:
+#    nota=int(input(f"Ingrese nota del alumno {cont}: "))
+#    suma=suma+nota
+#    cont=cont+1
+#print(f"El promedio de las notas es {suma/cont}")
+
+#For
+#cadena="Python"
+#letra = ' '
+#if 'o' in cadena:
+#    letra = 'o'
+#    for letra in cadena:
+#        print(letra)
+
+#Range
+num=2
+if num in range(3):
+    print(num)
