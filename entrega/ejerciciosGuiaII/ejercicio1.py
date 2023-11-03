dcf7f42cb2f3918a9aaa2ef8ffb5fc3d1006d020
Tankybara <147439760+Tankybara@users.noreply.github.com>
2023-11-03 21:10:57 +0000

    iaju

---
+++ entrega/ejerciciosGuiaII/ejercicio1.py
@@ -0,0 +1,5 @@
+edad = int(input("Ingrese su edad: "))
+if(edad >= 18):
+    print("Tiene edad suficiente para conducir.")
+else:
+    print("Faltan",18-edad,"17años para tener la edad suficiente para conducir.")
