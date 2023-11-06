cef56623d61d81753d4e8ea078509f8b58523345
Tankybara <147439760+Tankybara@users.noreply.github.com>
2023-11-06 19:35:46 +0000

    loop

--- entrega/iteraciones.py
+++ entrega/iteraciones.py
@@ -1,26 +1,56 @@
-#cont=0
-#while cont<=10:
-#    print(cont)
-#    cont=cont+1
+#contpar=0
+#contimpar=0
+#for _ in range(20):
+#    num=int(input("Ingrese num: "))
+#   if num%2==0:
+#        contpar=contpar+1
+#    else:
+#        contimpar=contimpar+1
+#print(f"Cantidad de numeros pares: {contpar}")
+#print(f"Cantidad de numeros impares: {contimpar}")
 
-#Promedio de notas
+#num1=int(input("Ingrese num1: "))
+#num2=int(input("Ingrese num2: "))
+#num3=int(input("Ingrese num3: "))
+#producto = num1*num2*num3
+#sumatoria= num1+num2+num3
+#print(f"La suma es {sumatoria}, y el producto es {producto}")
+
+#Con iteracion While
+#num=int(input("Ingrese un numero: "))
 #cont=1
 #suma=0
-#while cont <= 3:
-#    nota=int(input(f"Ingrese nota del alumno {cont}: "))
-#    suma=suma+nota
+#prod=1
+#while cont<=3:
+#    suma=suma+num
+#    prod=prod*num
+#    num=int(input("Ingrese otro numero: "))
 #    cont=cont+1
-#print(f"El promedio de las notas es {suma/cont}")
+#print(f"La suma es {suma}, y el producto es {prod}")
 
-#For
-#cadena="Python"
-#letra = ' '
-#if 'o' in cadena:
-#    letra = 'o'
-#    for letra in cadena:
-#        print(letra)
+#Con iteracion For
+#suma=0
+#prod=1
+#for _ in range(3):
+#    num=int(input("Ingrese otro numero: "))
+#    suma=suma+num
+#    prod=prod*num
+#print(f"La suma es {suma}, y el producto es {prod}")
 
-#Range
-num=2
-if num in range(3):
-    print(num)
+#Numero menor con iteracion For
+#menor=int(input("Ingrese un numero: "))
+#for _ in range(4):
+#    num=int(input("Ingrese un numero: "))
+#    if num < menor:
+#        menor = num
+#print(f"El menor de cinco numeros es {menor}")
+
+#Numero menor con iteracion While
+menor=int(input("Ingrese un numero: "))
+cont=1
+while cont<=4:
+    num=int(input("Ingrese un numero: "))
+    if num < menor:
+        menor = num
+    cont=cont+1
+print(f"El menor de todos los numeros es {menor}")
