de320ef4f0ef8a6fb56af741f66eeca7f9e5c9aa
Tankybara <147439760+Tankybara@users.noreply.github.com>
2023-11-06 19:53:29 +0000

    slice

---
+++ entrega/slicing.py
@@ -0,0 +1,10 @@
+texto = "AIPython"
+#copyText = texto[:]
+#texto = texto[0:3]
+#print(texto)
+#print(copyText)
+#for letra in texto:
+#    print(letra,end=',')
+#for i in range(len(texto)):
+#    print(texto[i])
+print(texto[::-1])
