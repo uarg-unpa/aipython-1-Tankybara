a116a3a25ebb64a829fc4a554ac3da0ea1cd51d7
Tankybara <147439760+Tankybara@users.noreply.github.com>
2023-11-29 22:48:49 +0000

    printed

---
+++ entrega/EjerciciosGuiaI/Ejercicio14.py
@@ -0,0 +1,5 @@
+pal1 = "Una ambiciosa"
+pal2 = "Introduccion"
+pal3 = "a Python"
+pal4 = "Parte 1"
+print(f"{pal1} {pal2} {pal3} {pal4}")
---
+++ entrega/EjerciciosGuiaI/Ejercicio16.py
@@ -0,0 +1,7 @@
+pal = "sometimes it is the people no one imagines anything of who do the things that no one can imagine."
+pal = pal.capitalize()
+print(pal)
+pal = pal.title()
+print(pal)
+pal = pal.swapcase()
+print(pal)
