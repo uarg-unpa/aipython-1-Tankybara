a57986160f457f2a4675c931373834da632bcc61
Tankybara <147439760+Tankybara@users.noreply.github.com>
2023-11-29 21:31:55 +0000

    In List

---
+++ entrega/ejerciciosGuiaIV/ejercicio6.py
@@ -0,0 +1,12 @@
+datosPersonales = []
+nombre = "Maximiliano"
+edad = 21
+altura = 169
+estadoCiv = "Soltero"
+direccion = "Para Adelante"
+datosPersonales.append(nombre)
+datosPersonales.append(edad)
+datosPersonales.append(altura)
+datosPersonales.append(estadoCiv)
+datosPersonales.append(direccion)
+print(datosPersonales)
